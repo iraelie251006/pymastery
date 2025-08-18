b3e499226dd102c6fce89e0c464f3f4b94096e6e
Elie <niyubwayoiraelie5777@gmail.com>
2025-08-18 23:10:53 +0200

    Refactor two_sum function and add additional code snippet.

--- twoSum.py
+++ twoSum.py
@@ -1,9 +1,17 @@
 def two_sum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    else:
-        return []
+     for i in range(len(nums)):
+         for j in range(i + 1, len(nums)):
+             if nums[i] + nums[j] == target:
+                 return [i, j]
+     else:
+         return []
 
 print(two_sum([1, 2, 3, 4, 5], 7))
+
+from collections import Counter
+s = "leetcode"
+counts = Counter(s)   # internally uses a HashMap
+for i, ch in enumerate(s):
+    if counts[ch] == 1:
+        print(i)  # index of first unique char
+        break
